0419f9d139eaef8a02a2fcb7cb8a59b3d1390778
krrsalmane <karroumsalmane@gmail.com>
2026-03-12 10:53:13 +0000

    create an Itinerary

--- app/Http/Controllers/ItineraryController.php
+++ app/Http/Controllers/ItineraryController.php
@@ -16,7 +16,25 @@ public function index()
 }
    
 
-    
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'titre'          => 'required|string|max:255',
+            'categorie'      => 'required|string|max:255',
+            'duration'       => 'required|integer|min:1',
+            'image'          => 'required|string|url',  
+            'destinations'   => 'required|array|min:2',
+            'destinations.*' => 'required|string|max:255',
+        ]);
+        $itinerary = Itinerary::create([
+            'titre'     => $data['titre'],
+            'categorie' => $data['categorie'],
+            'duration'  => $data['duration'],
+            'image'     => $data['image'],
+            'user_id'   => Auth::user()->id,
+        ]);
+        return response()->json(['data' => $itinerary], 201);
+    }
    
 
 
